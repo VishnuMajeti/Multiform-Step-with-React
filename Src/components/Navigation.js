d4cfe2bbc5282d9905f467aa7bc031ee7ee7bbe4
VishnuMajeti <139220768+VishnuMajeti@users.noreply.github.com>
2024-07-11 15:10:07 +0530

    Navigation.js

--- Src/components/Navigation.js
+++ Src/components/Navigation.js
@@ -1,1 +1,15 @@
+// src/components/Navigation.js
+import React from 'react';
+
+const Navigation = ({ currentStep, navigateToStep }) => {
+  return (
+    <div>
+      <button disabled={currentStep === 1} onClick={() => navigateToStep(1)}>Step 1</button>
+      <button disabled={currentStep === 2} onClick={() => navigateToStep(2)}>Step 2</button>
+      <button disabled={currentStep === 3} onClick={() => navigateToStep(3)}>Step 3</button>
+    </div>
+  );
+};
+
+export default Navigation;
 
